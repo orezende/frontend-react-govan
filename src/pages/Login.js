d49ac6123fa8db92e1eb0c32a99955c73a1a2039
João Vitor Rezende <Joao.Rezende@unisys.com>
2019-08-22 10:45:29 -0300

    efetuado login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-
 import Logo from '../assets/Logo.svg';
+
+import {login} from '../services/auth'
+
+import api from '../services/api';
 
 import './Login.css';
 
@@ -13,18 +16,28 @@ class Login extends Component {
         error: ""
     };
 
-    handleLogin = e => {
+    handleLogin = async e => {
         e.preventDefault();
-
-        alert("Logando");
-    }
+        const { email, password } = this.state;
+        if (!email || !password) {
+          this.setState({ error: "Preencha todos os campos para prosseguir!" });
+        } else {
+          try {
+            const response = await api.post("auth/user", { email, password });
+            login(response.data.token);
+            this.props.history.push("/app");
+          } catch (err) {
+            this.setState({error: err.response.data.error});
+          }
+        }
+      };
 
     render() {
         return (
             <div className="container">
                 <form onSubmit={this.handleLogin}>
                     <img src={Logo} alt="GoVan" />
-                    {this.state.error && <p>this.state.error</p>}
+                    {this.state.error && <p>{this.state.error}</p>}
 
                     <input
                         type="text"
@@ -33,7 +46,7 @@ class Login extends Component {
                     <input
                         type="password"
                         placeholder="Digite sua senha"
-                        onChange={e => this.setState({ email: e.target.value })} />
+                        onChange={e => this.setState({ password: e.target.value })} />
 
                     <button type="submit">Entrar</button>
                     <Link to="/register">Quero me cadastrar</Link>
